b70b9fc5ed2998d7bd31116a8392b689ef7de650
Hugh Willson <hugh@octonary.com>
2015-08-06 12:25:47 +0000

    Added Mandrill PHP API; wired up quick wp_mail pluggable override.

---
+++ wp-mandrill.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Plugin Name: Simple WP Mandrill
+ * Plugin URI: http://github.com/hwillson/wp-mandrill
+ * Description: Simple (and performant) Mandrill WordPress plugin.
+ * Version: 1.0.0
+ * Author: hwillson
+ * Author URI: http://github.com/hwillson/wp-mandrill
+ * License: MIT
+ */
+
+require_once 'lib/mandrill-api-php/src/Mandrill.php';
+
+define('MANDRILL_API_KEY', 'XXX');
+define('MANDRILL_FROM_EMAIL', 'name@email.com');
+define('MANDRILL_FROM_NAME', 'John Smith');
+
+if (!function_exists('wp_mail')) {
+
+  function wp_mail($to, $subject, $message, $headers = '') {
+
+    $mandrill = new Mandrill(MANDRILL_API_KEY);
+    $mandrill_msg = array(
+      'html' => $message,
+      'subject' => $subject,
+      'from_email' => MANDRILL_FROM_EMAIL,
+      'from_name' => MANDRILL_FROM_NAME
+    );
+
+    $emails = array();
+    if (is_array($to)) {
+      foreach ($to as $email) {
+        $emails[] = array(
+          'email' => $email
+        );
+      }
+    } else {
+      $emails[] = array(
+        'email' => $to
+      );
+    }
+    $mandrill_msg['to'] = $emails;
+
+    $mandrill->messages->send($mandrill_msg);
+
+  }
+
+}
